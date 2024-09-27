911284a38bba8853429fc0907b424f9d5c6fc1aa
Viet Nguyen <viethoang.nguyen@mail.utoronto.ca>
2024-09-27 10:34:04 -0400

    update simulate data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -10,17 +10,29 @@
 
 #### Workspace setup ####
 library(tidyverse)
+set.seed(123)
 # [...UPDATE THIS...]
 
 #### Simulate data ####
 num_samples <- 100
 start_date <- as.Date("2023-11-15")
 end_date <- as.Date("2024-03-23")
+mean_nb <- mean(raw_data$`Clients transported`)
+prob_nb <- 33/mean_nb 
+
 date_seq <- runif(n = num_samples, min = as.numeric(start_date), max = as.numeric(end_date))
 clients_stationary <- rpois(n = num_samples, lambda = 30)
+clients_transported <- rpois(n = num_samples, lambda = 1)
 clients <- rpois(n = num_samples, lambda = 39)
 
-simulated_data <- data.frame(dates = date_seq, clients = clients, clients_stationary=clients_stationary)
+simulated_data <- tibble(
+  dates = as.Date(
+    runif(
+      n = num_samples,
+      min = as.numeric(start_date),
+      max = as.numeric(end_date)
+    )),
+  clients_stationary, clients, clients_transported)
 
 ### Write_csv
 #install.packages("readr")
